6f398e6fb84038e5d7fac92aaab1b2a92290eda1
Konstantinos Livieratos <koslibpro@gmail.com>
2014-01-02 11:44:38 +0200

    Initial commit

---
+++ app/Controller/IssuesController.php
@@ -0,0 +1,140 @@
+<?php
+class IssuesController extends AppController {
+    public $helpers = array('Html', 'Form');
+    public $uses = array('DefaultModel', 'Issue');
+    var $components = array('Session');
+    
+    public function state($id=null) {
+        if ($this->request->is('get')) {
+            $this->redirect(array('action' => 'view', $id));
+        }
+        if ($this->Session->read('Auth.User.role') == 'admin') {
+            
+            $post = $this->Issue->findById($id);
+            if ($post['Issue']['state'] == 0) {
+                $post['Issue']['state'] = 1;
+            } else {
+                $post['Issue']['state'] = 0;
+            }
+            $this->Issue->create();
+            
+            $this->Issue->save($post);
+            $this->Session->setFlash(__(var_dump($post)));
+        }
+        $this->redirect(array('action' => 'view', $id));
+    }
+    
+    public function index($state=null) {
+        $this->layout = 'tracker';
+        if ($this->Session->read('Auth.User.username') != null) {
+            //$this->redirect(array('controller' => 'users', 'actions'=> 'login'));
+        }
+        if ($state == null) {
+            $this->set('posts', $this->Issue->findAllByCommentid(0, "*", array("id" => "DESC")));
+        } else {
+            $this->set('posts', $this->Issue->find('all',
+                                                   array('conditions' => array('Issue.commentid =' => '0',
+                                                                               'Issue.state =' => $state),
+                                                         'order' => array('Issue.id DESC'),
+                                                         )
+                                                   ));
+        }
+    }
+
+    public function view($id=null) {
+        
+        $this->layout = 'tracker';
+
+        $post = $this->Issue->findById($id);
+        $comments = $this->Issue->findAllByCommentid($id);
+        
+        if (!$post) { $this->redirect(array('action' => 'index')); }
+        
+        $this->set('post', $post);
+        $this->set('comments', $comments);
+    }
+    
+    public function add() {
+        $this->layout = 'tracker';
+        if ($this->Session->read('Auth.User.username') != null) {
+            if ($this->request->is('post')) {
+                $this->Issue->create();
+                $this->Issue->set("author", $this->Session->read('Auth.User.username'));
+                $this->Issue->set("state", 0);
+                //var_dump($this->Issue);
+                if ($this->Issue->save($this->request->data)) {
+                    $this->Session->setFlash(__('Your post has been saved.'));
+                    //return $this->redirect(array('action' => 'index'));
+                } else {
+                    $this->Session->setFlash(__('Unable to add your post.'));
+                }
+            }
+        } else {
+            //return $this->redirect(array('action' => 'index'));
+        }
+        
+    }
+    public function edit($id = null) {
+        $this->layout = 'tracker';
+        $post = $this->Issue->findById($id);
+        if ($post['Issue']['author'] == $this->Session->read('Auth.User.username')) {
+            
+            $this->set('post', $post);
+            if (!$post) {
+                $this->redirect(array('action' => 'index'));
+            }
+            if ($this->request->is('post') || $this->request->is('put')) {
+                $this->Issue->id = $id;
+                if ($this->Issue->save($this->request->data)) {
+                    $this->Session->setFlash(__('Your post has been updated.'));
+                    $this->redirect(array('action' => 'index'));
+                }
+                $this->Session->setFlash(__('Unable to update your post.'));
+            }
+            if (!$this->request->data) {
+                $this->request->data = $post;
+            }
+            
+        } else {
+            $this->redirect(array('action' => 'view', $id));
+        }
+    }
+    
+    public function delete($id) {
+        $this->layout = 'tracker';
+        if ($this->Session->read('Auth.User.username') != null) {
+            if ($this->request->is('get')) {
+                $this->redirect(array('action' => 'index'));
+            }
+            $post = $this->Issue->findById($id);
+            if ($post['Issue']['author'] == $this->Session->read('Auth.User.username')) {
+                if ($this->Issue->delete($id)) {
+                    $this->Session->setFlash(__('The post with id: %s has been deleted.', h($id)));
+                    $this->redirect(array('action' => 'index'));
+                }
+                
+            }
+        }
+    }
+    
+    public function comment($post_id = null) {
+        if ($this->request->is('post')) {
+            $form = $this->Issue->read(null, $post_id);
+            $form['Issue']['answers'] = $form['Issue']['answers'] + 1;
+            $this->Issue->create();
+            $this->Issue->save($form);   
+            $this->Issue->create();
+            $this->Issue->set(array('commentid'=>$post_id));
+            $this->Issue->set("author", $this->Session->read('Auth.User.username'));
+            $this->Issue->set("title", "comment");
+            if ($this->Issue->save($this->request->data)) {
+                $this->Session->setFlash('Your comment has been added.');
+                //$this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash('Unable to add your comment.');
+            }
+            $this->redirect(array('action' => 'view', $post_id));
+        }
+    }
+}
+?>
