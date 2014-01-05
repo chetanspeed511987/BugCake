71397c9bbafcc02d31c3dce7a7b35e49b7c38bd1
Kostas Livieratos <kostasl@lubbleup.com>
2014-01-05 12:18:30 +0200

    Added BugCake Plugin

---
+++ BugCake/Controller/BugCakeAppController.php
@@ -0,0 +1,6 @@
+<?php
+
+class ContactManagerAppController extends AppController {
+}
+
+?>
---
+++ BugCake/Model/Issue.php
@@ -0,0 +1,12 @@
+<?php
+class Issue extends BugCakeAppModel {
+    public $validate = array(
+        'title' => array(
+            'rule' => 'notEmpty'
+        ),
+        'body' => array(
+            'rule' => 'notEmpty'
+        )
+    );
+}
+?>
